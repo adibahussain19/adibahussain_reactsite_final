25086e56d5b1e06a1272bf6c5008885df86acece
Adiba Hussain <adibahussain@mac.myfiosgateway.com>
2024-11-12 00:30:13 -0500

    changed website title and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import WritingPage from "./pages/WritingPage.jsx";
 import ProjectsPage from "./pages/ProjectsPage.jsx";
 import RootLayout from "./pages/Root.jsx";
 
-
-
 const router = createBrowserRouter(
   [
     {
--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom/client";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import App from "./App.jsx";
-import "./index.css";
-
-const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
